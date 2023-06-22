a974e919aacafd4ae2266c0243d9d9e62aff197e
B33 <geoffrey.bii@student.moringaschool.com>
2023-06-22 12:44:04 +0300

    assignment submission

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+let message = document.getElementById('text')
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.querySelector('p');
+   console.log("JavaScript is so cool. It lets me add text to my page programmatically.")
+  message.textContent='This is really cool!';
+   });
+   
+    console.log(message)
